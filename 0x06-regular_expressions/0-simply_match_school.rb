40297ad31c2d5bbf4fda34ce89ab9892c5f4d285
Adamin88 <elhihi.mohamed@gmail.com>
2023-10-31 19:34:20 -0700

    updated

--- 0x06-regular_expressions/0-simply_match_school.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-arg = ARGV[0]
-
-pattern = /School/
-
-matches = arg.scan(pattern)
-
-puts "#{matches.join()}"
--- 0x06-regular_expressions/1-repetition_token_0.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-arg = ARGV[0]
-
-pattern = /hbtt{1,4}n/
-
-matches = arg.scan(pattern)
-
-puts "#{matches.join()}"
